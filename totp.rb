e2f76cce6d6c6e390f4ae331d07f46ae86b81064
groot <guyanbiao@gmail.com>
2018-09-13 17:00:41 +0800

    change variable name

--- totp.rb
+++ totp.rb
@@ -4,11 +4,18 @@ require 'byebug'
 
 MAX_OFFSET = 59
 
+#密码更新时间
+PERIOD = 60
+
+secret = "your_secret"
+
 digest = OpenSSL::Digest.new('sha512')
 
-data =  ((Time.now.to_i / 60).to_s 16)
+data =  ((Time.now.to_i / PERIOD).to_s 16).rjust(8, '0')
 
-hmac = OpenSSL::HMAC.digest(digest, "hello", data).unpack("C*")
+puts data
+
+hmac = OpenSSL::HMAC.digest(digest, secret, data).unpack("C*")
 
 offset = hmac[-1] & 0x3F;
 
@@ -16,4 +23,4 @@ offset = offset % MAX_OFFSET;
 
 bin_code = ((hmac[offset] & 0x7f) << 24) | (hmac[offset+1] & 0xff) << 16 | (hmac[offset+2] & 0xff) << 8 | (hmac[offset+3] & 0xff)
 
-puts bin_code % (10 ** 6);
+puts bin_code % (10 ** 6)
